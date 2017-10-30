7a78e2c8086d46b1db5eaace095fcf375be2e4d4
tymoorej <tymoore@ualberta.ca>
2017-10-30 15:53:35 -0600

    SCROLLING!!!! :)

--- Main.py
+++ Main.py
@@ -462,7 +462,85 @@ def customerMenu(customer):
             curMode = int(input("Select corresponding number: \n 1.Search\n 2.Order\n 3.List Orders\n 4.LogOff\n 5.Return\n"))
         elif curMode == SEARCH:
             #TODO: Tymoore add the search function call here
-            pass
+            keywords=input("Please enter your space seperated keywords: ")
+            results=search_for_keyword(keywords)
+            print()
+            print()
+            print()
+
+            LAYOUT = "{!s:15} {!s:25} {!s:20} {!s:15} {!s:25} {!s:15} {!s:25} {!s:15}"
+
+            if len(results)<5:
+                print(LAYOUT.format("Product ID","Product Name","Product Unit","# of Stores","# of Stores(in stock)","Min Price","Min Price(in stock)","Orders in last week"))
+                for i in range(len(results)):
+                    print(LAYOUT.format(*results[i]))
+                print()
+                print()
+                print()
+
+
+                while True:
+                    row_index = int(input("Select the number of the row you would like to know more about (NOTE row starts at 0): "))
+
+                    if(row_index>=len(results) or row_index<0):
+                        print("Sorry that row does not exist please try again")
+                    else:
+                        print(results[row_index][0])
+                        break
+            else:
+                times_moved=0
+                while True:
+                    minimum=min(times_moved*5+5,len(results))
+
+                    if(times_moved*5+5<len(results)):
+                        print()
+                        print()
+                        print()
+                        print(LAYOUT.format("Product ID","Product Name","Product Unit","# of Stores","# of Stores(in stock)","Min Price","Min Price(in stock)","Orders in last week"))
+                        for i in range(times_moved*5,minimum):
+                            print(LAYOUT.format(*results[i]))
+                        scroll=int(input("Select 1 to see more rows or 0 to examine these rows further: "))
+                        if scroll==1:
+                            times_moved=times_moved + 1
+                            continue
+                        elif scroll==0:
+                            pass
+                        else:
+                            should_continue=0
+                            while True:
+                                print("Please select either 0 or 1")
+                                scroll=int(input("Select 1 to see more rows or 0 to examine these rows further: "))
+                                if scroll==0:
+                                    break
+                                elif scroll==1:
+                                    times_moved=times_moved + 1
+                                    should_continue=1
+                                    break
+                            if should_continue:
+                                continue
+
+                    else:
+                        print()
+                        print()
+                        print()
+                        print(LAYOUT.format("Product ID","Product Name","Product Unit","# of Stores","# of Stores(in stock)","Min Price","Min Price(in stock)","Orders in last week"))
+                        for i in range(times_moved*5,len(results)):
+                            print(LAYOUT.format(*results[i]))
+
+                    row_index = int(input("Select the number of the row you would like to know more about (NOTE row starts at 0): "))
+
+                    minimum=min(times_moved*5+5,len(results))
+                    if(row_index>=(minimum-times_moved*5) or row_index<0):
+                        print("Sorry that row does not exist please try again")
+                    else:
+                        print(results[times_moved*5+row_index][0])
+                        break
+
+
+            print()
+            print()
+            print()
+            break
         elif curMode == ORDER:
             #TODO: Add order function call here
             pass
@@ -512,7 +590,6 @@ def main():
     setup()
     define_tables()
     insert_data()
-    print(search_for_keyword('oo tt'))
     loginScreen()
 
 
